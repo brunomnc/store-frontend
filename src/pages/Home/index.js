32c32aaf99f45fa7cbef1b883e0df8af70d56878
ZummyGummy <brunomnc@yahoo.com.br>
2019-08-07 13:54:38 -0300

    Minor api adjustments

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,108 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://www.bigbasket.com/media/uploads/p/l/10000063_20-fresho-broccoli.jpg"
-          alt="Brocolis"
-        />
-        <strong>Broccoli</strong>
-        <span>$50.00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add too Cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://www.bigbasket.com/media/uploads/p/l/10000063_20-fresho-broccoli.jpg"
-          alt="Brocolis"
-        />
-        <strong>Broccoli</strong>
-        <span>$50.00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add too Cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://www.bigbasket.com/media/uploads/p/l/10000063_20-fresho-broccoli.jpg"
-          alt="Brocolis"
-        />
-        <strong>Broccoli</strong>
-        <span>$50.00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add too Cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://www.bigbasket.com/media/uploads/p/l/10000063_20-fresho-broccoli.jpg"
-          alt="Brocolis"
-        />
-        <strong>Broccoli</strong>
-        <span>$50.00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add too Cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://www.bigbasket.com/media/uploads/p/l/10000063_20-fresho-broccoli.jpg"
-          alt="Brocolis"
-        />
-        <strong>Broccoli</strong>
-        <span>$50.00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add too Cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://www.bigbasket.com/media/uploads/p/l/10000063_20-fresho-broccoli.jpg"
-          alt="Brocolis"
-        />
-        <strong>Broccoli</strong>
-        <span>$50.00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add too Cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://www.bigbasket.com/media/uploads/p/l/10000063_20-fresho-broccoli.jpg"
-          alt="Brocolis"
-        />
-        <strong>Broccoli</strong>
-        <span>$50.00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add too Cart</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products', { crossdomain: true });
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.description} alt={product.name} />
+            <strong>{product.name}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
+              <span>Add too Cart</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
